670a55da078ef21eb4d104ab6a6508103a646b42
TanyaCST <schen@smith.edu>
2024-03-10 21:20:15 -0400

    house done, library almost, working on cafe

--- House.java
+++ House.java
@@ -1,12 +1,84 @@
 /* This is a stub for the House class */
-public class House {
 
-  public House() {
+import java.util.ArrayList;
+
+public class House extends Building {
+  private ArrayList<String> residents; // The <String> tells Java what kind of data we plan to store IN the ArrayList
+  private boolean hasDiningRoom;
+  boolean elevator;
+
+  public House(String name, String address, int nFloors, boolean hasDiningRoom, boolean elevator) {
+    super(name, address, nFloors);
+
+    this.residents = new ArrayList<String>();
+    this.hasDiningRoom = hasDiningRoom;
+    this.elevator = elevator;
+
+    if(hasDiningRoom == true){
+      System.out.println("There is a dining room in the house");
+    }
+    else{
+      System.out.println("there is no dining room in the house.");
+    }
     System.out.println("You have built a house: 🏠");
   }
 
+  /**
+   * 
+   * @return
+   */
+  public boolean hasDiningRoom(){
+    return hasDiningRoom;
+  }
+  
+  /**
+   * 
+   * @return
+   */
+  public int nResidents(){
+    return residents.size();
+  }
+
+  /**
+   * Add name into the residents arraylist
+   * @param name
+   */
+  public void moveIn(String name){
+    this.residents.add(name);
+  }
+  
+  /**
+   * return the name of the person who moved out
+   * @param name The name of residents who are moving out
+   * @return
+   */
+  public String moveOut(String name){
+    if (this.residents.contains(name)) {
+      this.residents.remove(name);
+      return name;
+    }
+    else{
+      return name + " isn't a resident in this house.";
+    }
+
+  }
+
+  public boolean IsResident(String name){
+    if (this.residents.contains(name)) {
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  public String toString(){
+    return this.name + " is a " + this.nFloors + "-story resident house located at " + this.address;
+  }
+
   public static void main(String[] args) {
-    new House();
+    House newHouse = new House("Northrop House", "49 Elm Street", 5, false, true);
+    System.out.println(newHouse.toString());
   }
 
 }
--- Library.java
+++ Library.java
@@ -1,12 +1,120 @@
 /* This is a stub for the Library class */
-public class Library {
 
-    public Library() {
+import java.util.Hashtable;
+
+public class Library extends Building {
+  private Hashtable<String, Boolean> collection;
+
+
+    public Library(String name, String address, int nFloors) {
+      super(name, address, nFloors);
+      this.collection = new Hashtable<String, Boolean>();
       System.out.println("You have built a library: 📖");
     }
   
+    /**
+     * To put a book into the library collection
+     * @param title
+     */
+    public void addTitle(String title){
+      this.collection.put(title, true);
+    }
+
+    /**
+     * To remove a book from the library collection
+     * @param title a string variable representing the title of book
+     * @return
+     */
+    public String removeTitle(String title){
+      if (collection.keySet().contains(title)){
+        this.collection.remove(title);
+        return title;
+      }
+      else{
+        //return title + "is not found in the library";
+        throw new NullPointerException();
+      }
+    }
+
+    /**
+     * To check out a book when it is borrowed by someone
+     * @param title a string variable representing the title of book
+     */
+    
+    public void checkOut(String title){
+      if (this.collection.keySet().contains(title)){
+        this.collection.replace(title, false);
+      }
+      else{
+        throw new NullPointerException();
+      }
+    }
+    
+    /**
+     * To check in a book when someone returns it back
+     * @param title a string variable representing the title of book
+     */
+    public void returnBook(String title){
+      if (this.collection.keySet().contains(title)){
+        this.collection.replace(title, true);
+      }
+      else{
+        throw new NullPointerException();
+      }
+    }
+
+    /**
+     * Check whether the collection contains certain book
+     * @param title a string variable representing the title of book
+     * @return
+     */
+    public boolean containsTitle(String title){
+      if (this.collection.keySet().contains(title)){
+        return true;
+      }
+      else{
+        return false;
+      }
+    } 
+
+    /**
+     * Check whether the book is available in the collection
+     * @param title a string variable representing the title of book
+     * @return
+     */
+    public boolean isAvailable(String title){
+      if (containsTitle(title) == true) {
+        if(this.collection.get(title) == true){
+          return true;
+        }
+        else{
+          return false;
+        }
+      }
+      else{
+        throw new NullPointerException();
+      }
+    }
+
+     /**
+     * Print outthe entire collection with check out status
+     */
+    public void printCollection(){
+      String itemInCollection;
+      for (int i = 0; i < this.collection.size(); i++){
+        
+        //itemInCollection += this.collection.get;
+      }
+    } // prints out the entire collection in an easy-to-read way (including checkout status)
+
     public static void main(String[] args) {
-      new Library();
+      Library mybibli = new Library("Fake Library", "123 Ghost Street", 100);
+      mybibli.collection.put("A Story", true);
+      mybibli.collection.put("Emily's Secret Diary", true);
+      mybibli.checkOut("A Story");
+      mybibli.returnBook("A Story");
+      System.out.println(mybibli.removeTitle("A Story"));
+      System.out.println(mybibli.collection);
     }
   
   }
